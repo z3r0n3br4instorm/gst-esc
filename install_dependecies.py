89ecc83b30d21e8524c68cf4561583780d9af90e
Ometh Abeyrathne <ometha12@gmail.com>
2023-10-03 22:39:23 +0530

    Update install_dependecies.py

--- install_dependecies.py
+++ install_dependecies.py
@@ -1,1 +1,10 @@
-d
+import os
+def install_start():
+  print("Seraphics Systems : Dependencies Installer...")
+  os.system("pip install mediapipe")
+  os.system("pip install pyqt5")
+  os.system("pip install cv2")
+  os.system("pip install doctest")
+
+if __name__ == "__main__":
+  install_start()
